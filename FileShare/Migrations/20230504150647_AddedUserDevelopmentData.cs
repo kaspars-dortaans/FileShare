79d1954049c94b6cfabb351bb1f890cee60c96f0
Kaspars Dortāns <59746217+kaspars-dortaans@users.noreply.github.com>
2023-05-04 19:55:41 +0300

    Added development data about admin user

---
+++ FileShare/Migrations/20230504150647_AddedUserDevelopmentData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileShare.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserDevelopmentData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "PasswordHash", "Role", "Username" },
+                values: new object[] { 1, "Admin", "Test", "$2a$11$Mprpo/PxbtIDUx/UIhX89e1L2GgPEiu0w4yZ5KDor1ECPaZRE789O", 0, "Admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
--- FileShare/Models/DataContext.cs
+++ FileShare/Models/DataContext.cs
@@ -1,5 +1,6 @@
 namespace FileShare.Models;
 
+using FileShare.Enums;
 using Microsoft.EntityFrameworkCore;
 
 public class DataContext : DbContext
@@ -7,6 +8,13 @@ public class DataContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<File> Files { get; set; }
     public DbSet<Setting> Settings { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Development data seeding
+        builder.Entity<User>().HasData(new User() { Id = 1, FirstName = "Admin", LastName = "Test", Role = Role.Admin, Username = "Admin", PasswordHash = "$2a$11$Mprpo/PxbtIDUx/UIhX89e1L2GgPEiu0w4yZ5KDor1ECPaZRE789O" });
+        //Password: "Test123"
+    }
 
     public DataContext(DbContextOptions<DataContext> options): base(options)
     { }
